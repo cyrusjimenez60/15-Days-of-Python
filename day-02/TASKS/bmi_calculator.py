53a1671c04784186696c16805c9e99ca6e6f9cf8
cyrusjimenez60 <cyrusjimenez60@gmail.com>
2024-03-08 08:19:46 +0800

    commit for task 2

--- day-02/TASKS/bmi_calculator.py
+++ day-02/TASKS/bmi_calculator.py
@@ -10,30 +10,25 @@ ABOVE 30.0          OBESITY
  
 # Write the code ↓ to read user's height and weight.
 # Be cautious when reading input of various data types.
+print("BMI CALCULATOR FOR ALF\n")
 
-
-
-
-
-
-
-
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
 
 # Write the code ↓ to perform the calculations of user's BMI and categorize its status.
+bmi = weight / (height ** 2)
 
-
-
-
-
-
+if bmi < 18.5:
+    status = "underweight"
+elif bmi >= 18.5 and bmi <= 24.9:
+    status = "normal weight"
+elif bmi >= 25 and bmi <= 29.9:
+    status = "overweight"
+else:
+    status = "obese"
 
 # Write the code ↓ to display the user's BMI and its classification.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 # Use :.2f format specifier when printing the BMI value to display the BMI with only two decimal places.
-
-
-
-
-
-
-
+print("\nHEIGHT: {} - WEIGHT: {}".format(height, weight))
+print("BMI: %.2f - NUTRITIONAL STATUS: %s" % (bmi, status))
--- day-02/TASKS/simple_calculator.py
+++ day-02/TASKS/simple_calculator.py
@@ -1,29 +1,25 @@
+import sys
 # Write the code ↓ to read user's input for two operands and selected operation.
 # NOTE: The two operands must be read as floats.
+print("SIMPLE CALCULATOR FOR ALF\n")
 
-
-
-
-
-
-
-
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, x, /): ")
 
 # Write the code ↓ to perform the calculations based on the selected operation.
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "x":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    print("\nEnter a valid operator from the choices only. Run the program again.")
+    sys.exit()
 
-
-
-
-
-
-
- 
 # Write the code ↓ to display the result.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
-
-
-
+print("\nThe result of {} {} {} is {}".format(num1, operator, num2, result))
