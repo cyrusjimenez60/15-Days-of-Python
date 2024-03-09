44a9e8b051d36d7fe11a7228f22e1d304937518d
cyrusjimenez60 <cyrusjimenez60@gmail.com>
2024-03-09 08:33:51 +0800

    Commit for Task 3

--- day-03/TASKS/factorial_calculator.py
+++ day-03/TASKS/factorial_calculator.py
@@ -1,22 +1,14 @@
 # Write the code ↓ to read the user's input for a non-negative integer.
 # Be cautious when reading input of various data types.
+print("FACTORIAL CALCULATOR FOR ALF\n")
 
-
-
-
-
+num = int(input("Please enter a non-negative integer: "))
 
 # Write the code ↓ to calculate the factorial of the user-defined integer using a loop.
-
-
-
-
-
+result = 1
+for i in range(1, num + 1, 1):
+    result *= i
 
 # Write the code ↓ to display the factorial result.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
-
+print("The factorial of {} is {}".format(num, result))
--- day-03/TASKS/perfect-number_determinator.py
+++ day-03/TASKS/perfect-number_determinator.py
@@ -1,28 +1,23 @@
 # Write the code ↓ to read the user's input for a positive integer.
 # Be cautious when reading input of various data types.
+print("PERFECT NUMBER DETERMINATOR FOR ALF\n")
 
-
-
-
-
-
-
-
-
+while (True):
+    num = int(input("Please enter a positive integer: "))
+    if num > 0:
+        break
+    else:
+        print("Please enter a non-negative integer only. Try again.\n")
 
 # Write the code ↓ to check if the entered integer is a Perfect Number using a loop.
-
-
-
-
-
-
-
+sum = 0
+for i in range(1, num, 1):
+    if (num % i) == 0:
+        sum += i
+                
 # Write the code ↓ to display the Perfect Number check result.
 # NOTE : You can use if-else statement or ternary operator to display the result.
-
-
-
-
-
-
+if sum == num:
+    print(f"{num} is a Perfect Number")
+else:
+    print(f"{num} is not a Perfect Number")
