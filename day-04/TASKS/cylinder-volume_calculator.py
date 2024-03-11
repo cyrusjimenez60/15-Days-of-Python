356aa5086deccd2fbaeff7db475453ab2c22e3a6
cyrusjimenez60 <cyrusjimenez60@gmail.com>
2024-03-11 12:04:44 +0800

    Commit for task 4

--- day-04/TASKS/cylinder-volume_calculator.py
+++ day-04/TASKS/cylinder-volume_calculator.py
@@ -2,24 +2,16 @@ import math
 
 # Write the code ↓ to read the radius and height of a cylinder from the user.
 # Be cautious when reading input of various data types.
+print("CYLINDER VOLUME CALCULATOR FOR ALF\n")
 
-
-
-
-
+radius = float(input("Please, enter the radius of the cylinder: "))
+height = float(input("Please, enter the height of the cylinder: "))
 
 # Write the code ↓ to calculate the volume of the cylinder using the formula V = πr^2h.
 # Formula to calculate the volume (V) of a cylinder:
 # V = π * r^2 * h
-
-
-
-
+vol = math.pi * math.pow(radius, 2) * height
 
 # Write the code ↓ to display the calculated volume with 2 decimal places.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
-
+print("The volume of the cylinder is: %.2f" % (vol))
--- day-04/TASKS/hypotenuse_calculator.py
+++ day-04/TASKS/hypotenuse_calculator.py
@@ -2,25 +2,15 @@ import math
 
 # Write the code ↓ to read the lengths of the two shorter sides of a right-angled triangle from the user.
 # Be cautious when reading input of various data types.
+print("HYPOTENUSE CALCULATOR FOR ALF\n")
 
-
-
-
-
+side_a = float(input("Please, enter the length of side A: "))
+side_b = float(input("Please, enter the length of side B: "))
 
 # Write the code ↓ to calculate the hypotenuse using the Pythagorean theorem.
 # The Pythagorean theorem: c^2 = a^2 + b^2, where c is the hypotenuse.
-
-
-
-
-
+hypo = math.sqrt(math.pow(side_a, 2) + math.pow(side_b, 2))
 
 # Write the code ↓ to display the calculated hypotenuse.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
-
-
+print("The hypotenuse of the right-angled triangle is: %.2f" % (hypo))
