6bf3be73631f00dc258e3d2341391e5d39cd2fd7
cyrusjimenez60 <cyrusjimenez60@gmail.com>
2024-03-11 23:10:47 +0800

    Commit for task 5

--- day-05/TASKS/check_palindrome.py
+++ day-05/TASKS/check_palindrome.py
@@ -1,25 +1,17 @@
 # Write the code ↓ to prompt the user to enter a word.
 # Be cautious when reading input of various data types.
+print("PALINDROME CHECKER FOR ALF\n")
 
-
-
-
-
-
+word = input("Please, enter a word/s to check: ")
 
 # Write the code ↓ to check if the entered word is a palindrome.
 # Utilize string functions to compare the original word with its reverse.
-
-
-
-
-
+loweredCaseWord = word.lower()
+reversedWord = loweredCaseWord[::-1]
 
 # Write the code ↓ to display whether the entered word is a palindrome or not.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
-
-
+if reversedWord == loweredCaseWord:
+    print("'{}' is a palindrome.".format(word))
+else:
+    print("'{}' is not a palindrome.".format(word))
--- day-05/TASKS/vowels_counter.py
+++ day-05/TASKS/vowels_counter.py
@@ -1,27 +1,17 @@
 # Write the code ↓ to prompt the user to enter a word.
 # Be cautious when reading input of various data types.
+print("VOWEL COUNTER FOR ALF\n")
 
-
-
-
-
-
+userInput = input("Please enter a word/s to check: ")
 
 
 # Write the code ↓ to count the number of vowels in the entered word.
 # Utilize string functions to iterate through the characters and identify vowels.
-
-
-
-
-
-
+vowelCount = 0
+for i in userInput:
+    if i in 'aeiouAEIOU':
+        vowelCount += 1
 
 # Write the code ↓ to display the count of vowels in the word.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-        
-
-
-
-
-
+print("The number of vowels in '{}' is: {}".format(userInput, vowelCount))     
